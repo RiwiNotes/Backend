9b6d927008ee78ac3596708b9a5e475467349e01
DeklanBeckett <juandausuga16@gmail.com>
2024-05-01 19:12:21 -0500

    solucione el tema de relaciones

--- Data/DataBaseContext.cs
+++ Data/DataBaseContext.cs
@@ -1,13 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using Backend.Models;
 
-namespace Backend.Data{
-    public class DataBaseContext : DbContext{
-        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options){
-
+namespace Backend.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
         }
 
-        public  DbSet<Categoria> Categorias { get; set;}
-        public  DbSet<Nota> Notas { get; set;}
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Nota> Notas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Nota>()
+                    .HasOne(n => n.categoria) // Corrige la propiedad de navegación aquí
+                    .WithMany(c => c.notas) // Corrige la propiedad de navegación aquí
+                    .HasForeignKey(n => n.categoria_id);
+            }
+
+
     }
 }
