794385fea4364e63354690862a20d231073c562b
DeklanBeckett <juandausuga16@gmail.com>
2024-05-01 19:17:45 -0500

    se soluciona otro error

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,6 +4,6 @@ namespace Backend.Models
     {
         public int id { get; set; }
         public string nombre_categoria { get; set; }
-        public List<Nota> notas { get; set; } // Cambia la propiedad a una colección de notas
+        public List<Nota> notas { get; set; } 
     }
 }
